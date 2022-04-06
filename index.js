a411112b74ff3808bf8dc358c09c2dcdd3609611
Judith Ramirez <rqmirez@gmail.com>
2022-04-06 16:20:08 -0400

    commit tree 0e83c2da307083723014a5578589778583281da7

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(feet){
+  if(feet <= 400)
+    return 'This one is on me!'
+  
+  else if(feet < 2000)
+    return `That will be twenty bucks.`
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+  else if(feet > 2000 && feet <2500)
+    return `I will gladly take your thirty bucks.`
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+  else if(feet >= 2500);{
+    return `No can do.`
+  }
+}
+function ternaryCheckCity(destination){
+  return destination == 'NYC' ? 'Ok, sounds good.' : 'No go.'
+}
+function switchOnCharmFromTip(tip){
+  let response;
+switch(tip){
+  case 'generous':
+    return`Thank you so much.`;
+    break;
+  case 'not as generous':
+    return`Thank you.`;
+    break;
+  default:
+    return 'Bye.'
+    break;
+}
 }
